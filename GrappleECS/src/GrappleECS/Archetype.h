799f82e82884d6ca5d30d4fa2c9d92ae704858bd
IgorLev91 <igorlevdansky91@gmail.com>
2025-03-20 17:54:27 +0300

    Added move constructors

--- GrappleECS/src/GrappleECS/Archetype.h
+++ GrappleECS/src/GrappleECS/Archetype.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "GrappleECS/Component.h"
-#include "GrappleECS/EntityStorage.h"
+#include "GrappleECS/EntityStorage/EntityStorage.h"
 
 #include <vector>
 #include <optional>
@@ -21,15 +21,30 @@ namespace Grapple
 	struct Archetype
 	{
 		size_t Id;
+
 		std::vector<ComponentId> Components; // Sorted
 		std::vector<size_t> ComponentOffsets;
 
 		std::unordered_map<ComponentId, ArchetypeEdge> Edges;
+
+		Archetype()
+			: Id(INVALID_ARCHETYPE_ID) {}
+
+		Archetype(Archetype&& other) noexcept
+			: Id(other.Id), Components(std::move(other.Components)), ComponentOffsets(std::move(other.ComponentOffsets)), Edges(std::move(other.Edges))
+		{
+			other.Id = INVALID_ARCHETYPE_ID;
+		}
 	};
 	
 	struct ArchetypeRecord
 	{
 		Archetype Data;
 		EntityStorage Storage;
+
+		ArchetypeRecord() {}
+
+		ArchetypeRecord(ArchetypeRecord&& other) noexcept
+			: Data(std::move(other.Data)), Storage(std::move(other.Storage)) {}
 	};
 }
