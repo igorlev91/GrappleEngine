a20708b3706772ed6b33989b8aaf942e538d3fc8
IgorLev91 <igorlevdansky91@gmail.com>
2025-05-02 15:32:26 +0300

    Moved mesh shader to RendererAssets & refactored

--- Packages/RendererAssets/Assets/Mesh.glsl
+++ Packages/RendererAssets/Assets/Mesh.glsl
@@ -29,15 +29,18 @@ void main()
 	o_Vertex.Normal = (normalTransform * vec4(i_Normal, 1.0)).xyz;
 	o_Vertex.Color = u_InstanceData.Color;
 	o_Vertex.Roughness = u_InstanceData.Roughness;
-	o_Vertex.Position = i_Position;
 
-    gl_Position = u_Camera.ViewProjection * u_InstanceData.Transform * vec4(i_Position, 1.0);
+	vec4 position = u_Camera.ViewProjection * u_InstanceData.Transform * vec4(i_Position, 1.0);
+	o_Vertex.Position = (u_InstanceData.Transform * vec4(i_Position, 1.0)).xyz;
+
+    gl_Position = position;
 }
 
 #type fragment
 #version 450
 
 #include "Camera.glsl"
+#include "BRDF.glsl"
 
 layout(std140, binding = 1) uniform DirLight
 {
@@ -56,52 +59,11 @@ struct VertexData
 layout(location = 0) in VertexData i_Vertex;
 layout(location = 0) out vec4 o_Color;
 
-vec3 Fresnel_Shlick(vec3 baseReflectivity, vec3 V, vec3 H)
-{
-	return baseReflectivity + (1 - baseReflectivity) * pow(1 - max(0.0, dot(V, H)), 5);
-}
-
-const float pi = 3.1415926535897932384626433832795;
-const vec3 baseReflectivity = vec3(0.4);
-
-vec3 Diffuse_Lambertian(vec3 color)
-{
-	return color / pi;
-}
-
-float NDF_GGXThrowbridgeReitz(float alpha, vec3 N, vec3 H)
-{
-	float NdotH = max(dot(N, H), 0.0);
-	NdotH *= NdotH;
-
-	float numer = alpha * alpha;
-	float denom = NdotH * (numer - 1.0) + 1.0;
-	return numer / (denom * denom * pi);
-}
-
-float ShlickBeckmann(vec3 N, vec3 X, float k)
-{
-	float NdotX = max(0.0, dot(N, X));
-	return NdotX / max(0.0000001, NdotX * (1.0 - k) + k);
-}
-
-float ShlickGGX(float alpha, vec3 N, vec3 V, vec3 L)
-{
-	float k = alpha / 2.0;
-	return ShlickBeckmann(N, V, k) * ShlickBeckmann(N, L, k);
-}
-
-vec3 Specular_CookTorence(float alpha, vec3 N, vec3 V, vec3 L)
-{
-	vec3 H = normalize(V + L);
-	vec3 numer = NDF_GGXThrowbridgeReitz(alpha, N, H) * ShlickGGX(alpha, N, V, L) * Fresnel_Shlick(baseReflectivity, V, H);
-	float denom = 4 * max(0.0, dot(V, N)) * max(0.0, dot(L, N));
-
-	return numer / max(denom, 0.0000001);
-}
-
 void main()
 {
+	if (i_Vertex.Color.a <= 0.0001)
+		discard;
+
 	vec3 N = normalize(i_Vertex.Normal);
 	vec3 V = normalize(u_Camera.Position - i_Vertex.Position);
 	vec3 H = normalize(V + u_LightDirection);
