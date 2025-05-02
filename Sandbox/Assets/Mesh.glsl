e540449bb3711ed8ade476c4f3aa793add26ee4a
IgorLev91 <igorlevdansky91@gmail.com>
2025-05-02 15:15:49 +0300

    Fixed view direction calculation in mesh shader

--- Sandbox/Assets/Mesh.glsl
+++ Sandbox/Assets/Mesh.glsl
@@ -29,9 +29,11 @@ void main()
 	o_Vertex.Normal = (normalTransform * vec4(i_Normal, 1.0)).xyz;
 	o_Vertex.Color = u_InstanceData.Color;
 	o_Vertex.Roughness = u_InstanceData.Roughness;
-	o_Vertex.Position = i_Position;
 
-    gl_Position = u_Camera.ViewProjection * u_InstanceData.Transform * vec4(i_Position, 1.0);
+	vec4 position = u_Camera.ViewProjection * u_InstanceData.Transform * vec4(i_Position, 1.0);
+	o_Vertex.Position = (u_InstanceData.Transform * vec4(i_Position, 1.0)).xyz;
+
+    gl_Position = position;
 }
 
 #type fragment
@@ -58,11 +60,11 @@ layout(location = 0) out vec4 o_Color;
 
 vec3 Fresnel_Shlick(vec3 baseReflectivity, vec3 V, vec3 H)
 {
-	return baseReflectivity + (1 - baseReflectivity) * pow(1 - max(0.0, dot(V, H)), 5);
+	return baseReflectivity + (vec3(1.0) - baseReflectivity) * pow(1.0 - max(0.0, dot(V, H)), 5);
 }
 
 const float pi = 3.1415926535897932384626433832795;
-const vec3 baseReflectivity = vec3(0.4);
+const vec3 baseReflectivity = vec3(0.04);
 
 vec3 Diffuse_Lambertian(vec3 color)
 {
@@ -82,7 +84,7 @@ float NDF_GGXThrowbridgeReitz(float alpha, vec3 N, vec3 H)
 float ShlickBeckmann(vec3 N, vec3 X, float k)
 {
 	float NdotX = max(0.0, dot(N, X));
-	return NdotX / max(0.0000001, NdotX * (1.0 - k) + k);
+	return NdotX / max(0.000001, NdotX * (1.0 - k) + k);
 }
 
 float ShlickGGX(float alpha, vec3 N, vec3 V, vec3 L)
@@ -97,7 +99,7 @@ vec3 Specular_CookTorence(float alpha, vec3 N, vec3 V, vec3 L)
 	vec3 numer = NDF_GGXThrowbridgeReitz(alpha, N, H) * ShlickGGX(alpha, N, V, L) * Fresnel_Shlick(baseReflectivity, V, H);
 	float denom = 4 * max(0.0, dot(V, N)) * max(0.0, dot(L, N));
 
-	return numer / max(denom, 0.0000001);
+	return numer / max(denom, 0.000001);
 }
 
 void main()
