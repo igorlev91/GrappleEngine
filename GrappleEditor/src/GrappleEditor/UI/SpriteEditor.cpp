27e07e9e5e162fd14d226e49dfae61471b1319ab
IgorLev91 <igorlevdansky91@gmail.com>
2025-05-21 16:59:02 +0300

    SpriteEditor: Fix cursor flickering when resizing selection rect

--- GrappleEditor/src/GrappleEditor/UI/SpriteEditor.cpp
+++ GrappleEditor/src/GrappleEditor/UI/SpriteEditor.cpp
@@ -6,12 +6,15 @@
 
 namespace Grapple
 {
+    Grapple_IMPL_ENUM_BITFIELD(SpriteEditor::SelectionRectSide);
+
     bool SpriteEditor::OnImGuiRenderer()
     {
         bool result = false;
         ImGuiWindowFlags windowFlags = ImGuiWindowFlags_AlwaysHorizontalScrollbar
             | ImGuiWindowFlags_AlwaysVerticalScrollbar
-            | ImGuiWindowFlags_NoScrollWithMouse;
+            | ImGuiWindowFlags_NoScrollWithMouse
+            | ImGuiWindowFlags_NoMove;
 
         if (!ImGui::BeginChild("Viewport", ImVec2(0, 0), false, windowFlags))
         {
@@ -20,6 +23,12 @@ namespace Grapple
         }
 
         const Ref<Texture>& texture = m_Sprite->GetTexture();
+        if (!texture)
+        {
+            ImGui::EndChild();
+            return false;
+        }
+
         ImGuiWindow* window = ImGui::GetCurrentWindow();
 
         ImVec2 windowScroll = window->Scroll;
@@ -27,7 +36,7 @@ namespace Grapple
 
         ImVec2 textureSize = ImVec2(texture->GetWidth(), texture->GetHeight());
         float textureAspectRatio = textureSize.x / textureSize.y;
-        
+
         ImVec2 maxTextureSize = window->Size - window->ScrollbarSizes;
         ImVec2 scaledTextureSize = ImVec2(maxTextureSize.x, maxTextureSize.x / textureAspectRatio);
 
@@ -42,8 +51,195 @@ namespace Grapple
         }
 
         ImGui::Image((ImTextureID)texture->GetRendererId(), textureSize * m_Zoom, ImVec2(0, 1), ImVec2(1, 0));
+        bool imageHovered = ImGui::IsItemHovered();
+
+        ImRect imageRect = { ImGui::GetItemRectMin(), ImGui::GetItemRectMax() };
+
+        ImVec2 mousePositionTextureSpace = WindowToTextureSpace(mousePosition);
+        mousePositionTextureSpace.x = glm::round(mousePositionTextureSpace.x);
+        mousePositionTextureSpace.y = glm::round(mousePositionTextureSpace.y);
+
+        mousePositionTextureSpace.x = glm::clamp(mousePositionTextureSpace.x, 0.0f, textureSize.x);
+        mousePositionTextureSpace.y = glm::clamp(mousePositionTextureSpace.y, 0.0f, textureSize.y);
+
+        SelectionRectSide selectionSides = RenderSelectionRect(imageRect);
+
+        SelectionRectSide currentSelectionSides = selectionSides;
+        if (m_ResizedSides != SelectionRectSide::None)
+            currentSelectionSides = m_ResizedSides;
+
+        if (currentSelectionSides == SelectionRectSide::TopLeft || currentSelectionSides == SelectionRectSide::BottomRight)
+            ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeNWSE);
+        else if (currentSelectionSides == SelectionRectSide::BottomLeft || currentSelectionSides == SelectionRectSide::TopRight)
+            ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeNESW);
+        else if (HAS_BIT(currentSelectionSides, SelectionRectSide::Left) || HAS_BIT(currentSelectionSides, SelectionRectSide::Right))
+            ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeEW);
+        else if (HAS_BIT(currentSelectionSides, SelectionRectSide::Top) || HAS_BIT(currentSelectionSides, SelectionRectSide::Bottom))
+            ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeNS);
+
+        if (m_ResizedSides != SelectionRectSide::None)
+        {
+            if (ImGui::IsMouseDown(ImGuiMouseButton_Left))
+            {
+                if (m_ResizedSides == SelectionRectSide::TopLeft)
+                    m_SelectionStart = mousePositionTextureSpace;
+                else if (m_ResizedSides == SelectionRectSide::BottomRight)
+                    m_SelectionEnd = mousePositionTextureSpace;
+                else if (m_ResizedSides == SelectionRectSide::TopRight)
+                {
+                    m_SelectionEnd.x = mousePositionTextureSpace.x;
+                    m_SelectionStart.y = mousePositionTextureSpace.y;
+                }
+                else if (m_ResizedSides == SelectionRectSide::BottomLeft)
+                {
+                    m_SelectionStart.x = mousePositionTextureSpace.x;
+                    m_SelectionEnd.y = mousePositionTextureSpace.y;
+                }
+                else if (m_ResizedSides == SelectionRectSide::Top)
+                    m_SelectionStart.y = mousePositionTextureSpace.y;
+                else if (m_ResizedSides == SelectionRectSide::Right)
+                    m_SelectionEnd.x = mousePositionTextureSpace.x;
+                else if (m_ResizedSides == SelectionRectSide::Bottom)
+                    m_SelectionEnd.y = mousePositionTextureSpace.y;
+                else if (m_ResizedSides == SelectionRectSide::Left)
+                    m_SelectionStart.x = mousePositionTextureSpace.x;
+            }
+
+            if (ImGui::IsMouseReleased(ImGuiMouseButton_Left))
+            {
+                m_ResizedSides = SelectionRectSide::None;
+                ValidateSelectionRect();
+            }
+        }
+        else if (imageHovered)
+        {
+            if (ImGui::IsMouseDown(ImGuiMouseButton_Left))
+            {
+                if (selectionSides == SelectionRectSide::None)
+                {
+                    if (!m_SelectionStarted)
+                    {
+                        m_SelectionStart = mousePositionTextureSpace;
+                        m_SelectionStarted = true;
+                    }
+
+                    m_SelectionEnd = mousePositionTextureSpace;
+                }
+                else
+                {
+                    m_ResizedSides = selectionSides;
+                    ValidateSelectionRect();
+                }
+            }
+
+            if (ImGui::IsMouseReleased(ImGuiMouseButton_Left))
+            {
+                m_SelectionStarted = false;
+            }
+        }
 
         ImGui::EndChild();
         return result;
+    }
+
+    ImVec2 SpriteEditor::WindowToTextureSpace(ImVec2 windowSpace)
+    {
+        return ImVec2((windowSpace + ImGui::GetCurrentWindow()->Scroll) / m_Zoom);
+    }
+
+    ImVec2 SpriteEditor::TextureToWindowSpace(ImVec2 textureSpace)
+    {
+        return ImVec2(textureSpace * m_Zoom - ImGui::GetCurrentWindow()->Scroll);
+    }
+
+    SpriteEditor::SelectionRectSide SpriteEditor::RenderSelectionRect(ImRect imageRect)
+    {
+        ImGuiWindow* window = ImGui::GetCurrentWindow();
+        ImDrawList* drawList = window->DrawList;
+
+        ImU32 defaultColor = 0xffffffff;
+        ImU32 contrastColor = 0xff000000;
+
+        ImRect selectionBounds = { TextureToWindowSpace(m_SelectionStart), TextureToWindowSpace(m_SelectionEnd) };
+
+        if (selectionBounds.Max.x < selectionBounds.Min.x)
+            std::swap(selectionBounds.Min.x, selectionBounds.Max.x);
+        if (selectionBounds.Max.y < selectionBounds.Min.y)
+            std::swap(selectionBounds.Min.y, selectionBounds.Max.y);
+
+        ImRect selectionUIRectBounds = { selectionBounds.Min + imageRect.Min + window->Scroll, selectionBounds.Max + imageRect.Min + window->Scroll };
+
+        if (selectionUIRectBounds.GetWidth() != 0 && selectionUIRectBounds.GetHeight() != 0)
+        {
+            // Inner rect
+            drawList->AddRect(
+                selectionUIRectBounds.Min + ImVec2(1.0f, 1.0f),
+                selectionUIRectBounds.Max - ImVec2(1.0f, 1.0f),
+                contrastColor);
+        }
+
+        // Middle rect
+        drawList->AddRect(
+            selectionUIRectBounds.Min,
+            selectionUIRectBounds.Max,
+            defaultColor);
+
+        // Outer rect
+        drawList->AddRect(
+            selectionUIRectBounds.Min - ImVec2(1.0f, 1.0f),
+            selectionUIRectBounds.Max + ImVec2(1.0f, 1.0f),
+            contrastColor);
+
+        ImVec2 cornerSize = ImVec2(m_SelectionRectCornerSize, m_SelectionRectCornerSize);
+        SelectionRectSide result = SelectionRectSide::None;
+
+        ImRect scrolledSelectionBounds = { selectionBounds.Min + window->Scroll, selectionBounds.Max + window->Scroll };
+
+        float sideWidth = cornerSize.x + scrolledSelectionBounds.GetWidth();
+        float sideHeight = cornerSize.y + scrolledSelectionBounds.GetHeight();
+
+        ImVec2 topRightCorner = ImVec2(scrolledSelectionBounds.Max.x, scrolledSelectionBounds.Min.y);
+        ImVec2 bottomLeftCorner = ImVec2(scrolledSelectionBounds.Min.x, scrolledSelectionBounds.Max.y);
+
+        if (RenderSelectionSide("Top", scrolledSelectionBounds.Min - cornerSize / 2.0f, ImVec2(sideWidth, cornerSize.y)))
+            result |= SelectionRectSide::Top;
+        if (RenderSelectionSide("Left", scrolledSelectionBounds.Min - cornerSize / 2.0f, ImVec2(cornerSize.x, sideHeight)))
+            result |= SelectionRectSide::Left;
+        if (RenderSelectionSide("Right", topRightCorner - cornerSize / 2.0f, ImVec2(cornerSize.x, sideHeight)))
+            result |= SelectionRectSide::Right;
+        if (RenderSelectionSide("Bottom", bottomLeftCorner - cornerSize / 2.0f, ImVec2(sideWidth, cornerSize.y)))
+            result |= SelectionRectSide::Bottom;
+
+        ImVec2 corners[] =
+        {
+            selectionUIRectBounds.Min,
+            ImVec2(selectionUIRectBounds.Max.x, selectionUIRectBounds.Min.y),
+            selectionUIRectBounds.Max,
+            ImVec2(selectionUIRectBounds.Min.x, selectionUIRectBounds.Max.y),
+        };
+
+        for (size_t i = 0; i < 4; i++)
+        {
+            drawList->AddRectFilled(corners[i] - cornerSize / 2.0f, corners[i] + cornerSize / 2.0f, 0xffffffff);
+            drawList->AddRect(corners[i] - cornerSize / 2.0f, corners[i] + cornerSize / 2.0f, 0xff000000);
+        }
+
+        return !m_SelectionStarted ? result : SelectionRectSide::None;
+    }
+
+    bool SpriteEditor::RenderSelectionSide(const char* name, ImVec2 position, ImVec2 size)
+    {
+        ImGui::SetCursorPos(position);
+        ImGui::InvisibleButton(name, size);
+
+        return ImGui::IsMouseHoveringRect(ImGui::GetItemRectMin(), ImGui::GetItemRectMax());
+    }
+
+    void SpriteEditor::ValidateSelectionRect()
+    {
+        if (m_SelectionEnd.x < m_SelectionStart.x)
+            std::swap(m_SelectionStart.x, m_SelectionEnd.x);
+        if (m_SelectionEnd.y < m_SelectionStart.y)
+            std::swap(m_SelectionStart.y, m_SelectionEnd.y);
     }
 }
--- GrappleEditor/src/GrappleEditor/UI/SpriteEditor.h
+++ GrappleEditor/src/GrappleEditor/UI/SpriteEditor.h
@@ -3,11 +3,27 @@
 #include "Grapple/Renderer/Texture.h"
 #include "Grapple/Renderer/Sprite.h"
 
+#include "GrappleEditor/ImGui/ImGuiLayer.h"
+
 namespace Grapple
 {
     class SpriteEditor
     {
     public:
+        enum class SelectionRectSide
+        {
+            None = 0,
+            Top = 1,
+            Right = 2,
+            Bottom = 4,
+            Left = 8,
+
+            TopLeft = Top | Left,
+            TopRight = Top | Right,
+            BottomLeft = Bottom | Left,
+            BottomRight = Bottom | Right,
+        };
+
         SpriteEditor()
             : m_Sprite(nullptr) {}
 
@@ -16,8 +32,24 @@ namespace Grapple
 
         bool OnImGuiRenderer();
     private:
-        Ref<Sprite> m_Sprite;
+        ImVec2 WindowToTextureSpace(ImVec2 windowSpace);
+        ImVec2 TextureToWindowSpace(ImVec2 textureSpace);
+
+        SelectionRectSide RenderSelectionRect(ImRect imageRect);
+        bool RenderSelectionSide(const char* name, ImVec2 position, ImVec2 size);
+
+        void ValidateSelectionRect();
+    private:
+        Ref<Sprite> m_Sprite = nullptr;
+        SelectionRectSide m_ResizedSides = SelectionRectSide::None;
 
         float m_Zoom = 1.0f;
+        float m_SelectionRectCornerSize = 8.0f;
+
+        bool m_HasSelection = false;
+        bool m_SelectionStarted = false;
+
+        ImVec2 m_SelectionStart = ImVec2(0.0f, 0.0f);
+        ImVec2 m_SelectionEnd = ImVec2(0.0f, 0.0f);
     };
 }
