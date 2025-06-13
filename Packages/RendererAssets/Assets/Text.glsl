cf72cae55935242ad516d0fbd3e21e3619a22057
IgorLev91 <igorlevdansky91@gmail.com>
2025-06-13 17:42:45 +0300

    Fixed black outline around the text + text shader refactor

--- Packages/RendererAssets/Assets/Text.glsl
+++ Packages/RendererAssets/Assets/Text.glsl
@@ -1,5 +1,6 @@
 Type = 2D
 DepthTest = false
+Blending = Transparent
 
 #begin vertex
 #version 450
@@ -9,23 +10,15 @@ DepthTest = false
 layout(location = 0) in vec3 i_Position;
 layout(location = 1) in vec4 i_Color;
 layout(location = 2) in vec2 i_UV;
-layout(location = 3) in int i_EntityIndex;
 
-layout(location = 0) out vec2 UV;
-layout(location = 1) out vec4 Color;
-
-#ifdef OPENGL
-	layout(location = 2) out flat int EntityIndex;
-#endif
+layout(location = 0) out vec2 o_UV;
+layout(location = 1) out vec4 o_Color;
 
 void main()
 {
 	gl_Position = u_Camera.ViewProjection * vec4(i_Position, 1.0);
-	UV = i_UV;
-	Color = i_Color;
-#ifdef OPENGL
-	EntityIndex = i_EntityIndex;
-#endif
+	o_UV = i_UV;
+	o_Color = i_Color;
 }
 
 #end
@@ -33,47 +26,37 @@ void main()
 #begin pixel
 #version 450
 
-layout(location = 0) in vec2 UV;
-layout(location = 1) in vec4 Color;
-#ifdef OPENGL
-	layout(location = 2) in flat int EntityIndex;
-#endif
+layout(location = 0) in vec2 i_UV;
+layout(location = 1) in vec4 i_Color;
 
 layout(set = 1, binding = 0) uniform sampler2D u_MSDF;
 
 layout(location = 0) out vec4 o_Color;
 
-#ifdef OPENGL
-	layout(location = 2) out int o_EntityIndex;
-#endif
-
 const float pxRange = 2.0;
 
-float screenPxRange()
+float ScreenPxRange()
 {
     vec2 unitRange = vec2(pxRange) / vec2(textureSize(u_MSDF, 0));
-    vec2 screenTexSize = vec2(1.0) / fwidth(UV);
+    vec2 screenTexSize = vec2(1.0) / fwidth(i_UV);
     return max(0.5 * dot(unitRange, screenTexSize), 1.0);
 }
 
-float median(float r, float g, float b)
+float Median(float r, float g, float b)
 {
     return max(min(r, g), min(max(r, g), b));
 }
 
 void main() {
-    vec3 msd = texture(u_MSDF, UV).rgb;
-    float sd = median(msd.r, msd.g, msd.b);
-    float screenPxDistance = screenPxRange()*(sd - 0.5);
+    vec3 msd = texture(u_MSDF, i_UV).rgb;
+    float sd = Median(msd.r, msd.g, msd.b);
+    float screenPxDistance = ScreenPxRange() * (sd - 0.5);
     float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
 
 	if (opacity <= 0.001)
 		discard;
 
-    o_Color = mix(vec4(0.0), Color, opacity);
-#ifdef OPENGL
-	o_EntityIndex = EntityIndex;
-#endif
+    o_Color = vec4(i_Color.rgb, i_Color.a * opacity);
 }
 
 #end
